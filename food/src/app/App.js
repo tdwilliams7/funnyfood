4db00bbf7df1df85726ae8836428c87a6cc02411
Troy Williams <troywilliams@Troys-MBP.hsd1.mn.comcast.net>
2017-10-10 16:12:36 -0500

    changed a bunch of stuff. should have committed sooner.
    everything still works. Navbar centered now.

--- food/src/app/App.js
+++ food/src/app/App.js
@@ -15,84 +15,157 @@ class App extends Component {
   render() {
     return (
       <div className="App container-fluid">
-          <nav className="navbar navbar-fixed-top navbar-expand-md navbar-light bg-light">
-            <a className="navbar-brand" href="#">Food</a>
+          <nav className="navbar navbar-text navbar-expand-md navbar-light fixed-top navbar-right">
+
+            <a className="navbar-brand" href="#">Humorous Eats</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
 
+
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                  <a className="nav-link" href='#chicken'>Chicken<span className="sr-only">(current)</span></a>
+              <ul className="nav navbar-nav navbar-right">
+
+                <li className="nav-item">
+                  <a className="nav-link" >Recipes</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href='#beef'>Beef</a>
+                  <a className="nav-link" >Techniques</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link disabled">Fish</a>
+                  <a className="nav-link">Features</a>
                 </li>
+                <li className='nav-item'>
+                  <a className='nav-link'>OUR EDITORS</a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link'>KITCHEN ESSENTIALS</a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link'>PODCASTS</a>
+                </li>
+
               </ul>
             </div>
+
           </nav>
 
 
+
+        <div className='mainContent'>
         <div className='row'>
           <div className='col-md-6'>
             <div className='card'>
-              <img className='card-img-top' src='http://www.seriouseats.com/recipes/assets_c/2017/09/20170902-chicken-pot-pie-vicky-wasik40-thumb-1500xauto-439266.jpg' alt='chicken'></img>
-              <div className='card-body'>
-                <p className='card-text'>here is some fake text; I like food a lot</p>
-              </div>
+              <a href='http://www.seriouseats.com/2017/10/how-to-make-a-pear-galette.html'>
+              <img className='card-img-top' src='http://www.seriouseats.com/images/2017/09/20170911-pear-galette-vicky-wasik-7.jpg' alt='chicken'></img>
+              <h2 className='cardText'>How To Make A Better Pear Gallete With Better Pear Flavor<br /><br /><h6 className='author'>Stella Park</h6></h2>
+
+              </a>
+
             </div>
           </div>
           <div className='col-md-6'>
+            <div className='card' href='http://www.seriouseats.com/2017/10/the-food-labs-reading-list-joyce-chen-cook-book.html'>
+              <a href='http://www.seriouseats.com/2017/10/the-food-labs-reading-list-joyce-chen-cook-book.html'>
+              <img className='card-img-top' src='http://www.seriouseats.com/images/2017/10/book-a-day-7-Joyce-Chen-Cook-Book.jpg' alt='chicken'></img>
+              <h2 className='cardText'>The Food Lab's Reading List, Day 7: The Joyce Chen Cook Book</h2>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col-lg-12'>
+            <table className='table table-hover'>
+                <tr>
+                  <td><h2 className='sectionTitle'>Cooking Techniques</h2></td>
+                </tr>
+            </table>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col-sm-4'>
+            <div className="card">
+              <img className="card-img-top" src="http://www.seriouseats.com/assets_c/2017/09/20170729-cookies-and-cream-cookies-vicky-wasik-22-thumb-625xauto-439520.jpg" alt="Card image cap"></img>
+                <div className="card-body">
+                  <h5>BraveTart: I Heard You Like Cookies, so I Put Oreos in Your Cookies</h5>
+                  <p className='author'>Stella Parks</p>
+                  <a className='btn-primary btn' href='http://www.seriouseats.com/recipes/2017/09/chicken-pot-pie-biscuit-topping-recipe.html'>View Recipe</a>
+                </div>
+            </div>
+          </div>
+
+          <div className='col-sm-4'>
+            <div className="card">
+              <img className="card-img-top" src="http://www.seriouseats.com/images/2017/09/20170908-roasted-vegetables-vicky-wasik-carrots5.jpg" alt="Card image cap"></img>
+                <div className="card-body">
+                  <h5 >How to Roast Carrots and Parsnips</h5>
+                  <p className='author'>J. Kenji Lopex Alt</p>
+                  <br />
+                  <a className='btn-primary btn' href='http://www.seriouseats.com/recipes/2013/12/roasted-carrots-harissa-creme-fraiche-food-lab-recipe.html'>View Recipe</a>
+                </div>
+            </div>
+          </div>
+          <div className='col-sm-4'>
+            <div className="card">
+              <img className="card-img-top" src="http://www.seriouseats.com/images/2017/09/20170914-sweetened-condensed-milk-vicky-wasik-8.jpg" alt="Card image cap"></img>
+                <div className="card-body">
+                  <h5> Homemade Sweetened Condensed Milk: Better Than the Canned Stuff</h5>
+                  <p className='author'>Stella Parks</p>
+                  <a className='btn-primary btn' href='http://www.seriouseats.com/recipes/2017/10/bravetart-sweetened-condensed-milk-recipe.html'>View Recipe</a>
+                </div>
+            </div>
+        </div>
+        </div>
+
+      {/*second row*/}
+
+        <div className='row'>
+          <div className='col-sm-4'>
             <div className='card'>
-              <img className='card-img-top' src='http://www.seriouseats.com/recipes/assets_c/2017/09/20170902-chicken-pot-pie-vicky-wasik40-thumb-1500xauto-439266.jpg' alt='chicken'></img>
+              <img className='card-img-top' src='http://www.seriouseats.com/sponsored/images/2017/09/20170912-del-real-carnitas-stuffed-peppers.jpg' alt='roasted peppers'></img>
               <div className='card-body'>
-                <p className='card-text'>here is some fake text; I like food a lot</p>
+                <h5>For a Heartier Stuffed Pepper, Use Carnitas, Rice, and Beans</h5>
+                <p className='author'>sponsored</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-sm-4'>
+            <div className='card'>
+              <img className='card-img-top'src='http://www.seriouseats.com/images/2017/09/20170825-ohitashi-vicky-wasik-9.jpg' alt='rice bowl'></img>
+              <div className='card-body'>
+                <h5>How to Make Ohitashi: Easy Japanese Marinated Greens</h5>
+                <p className='author'>Daniel Gritzer</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-sm-4'>
+            <div className='card'>
+              <img className='card-img-top' src='http://www.seriouseats.com/images/2017/09/20170730-bravetart-apple-pie-vicky-wasik-1.jpg' alt='apple pie'></img>
+              <div className='card-body'>
+                <h5>BraveTart: How to Make Old-Fashioned Apple Pie, No Gimmicks Required</h5>
+                <p className='author'>Stella Parks</p>
               </div>
             </div>
           </div>
         </div>
 
-        <div className='chicken'>
         <div className='row'>
-        <div className='col-xs-12'>
-          <a name='chicken'></a>
-          <h2>Chicken</h2>
-          <hr></hr>
-        </div>
-        <div className='row'>
-          <div className='col-sm-4'>
-            <div className="card">
-              <img className="card-img-top" src="http://www.seriouseats.com/recipes/assets_c/2017/09/20170902-chicken-pot-pie-vicky-wasik40-thumb-1500xauto-439266.jpg" alt="Card image cap"></img>
-                <div className="card-body">
-                  <p className="card-text">Some quick example textfdslfbusbfusbd;kfh;df di fs iofhsd o;fhsd ifhds fuh ihfp oihiofh[ofiheqfo[qoih[ ih[hto build on the card title and make up the bulk of the card's content.</p>
-                  <a className='btn-primary btn' href='http://www.seriouseats.com/recipes/2017/09/chicken-pot-pie-biscuit-topping-recipe.html'>View Recipe</a>
-                </div>
-            </div>
-          </div>
+          <div className='col-lg-12'>
+            <table className='table moreLink'>
 
-          <div className='col-sm-4'>
-            <div className="card">
-              <img className="card-img-top" src="http://www.seriouseats.com/recipes/assets_c/2017/09/20170902-chicken-pot-pie-vicky-wasik40-thumb-1500xauto-439266.jpg" alt="Card image cap"></img>
-                <div className="card-body">
-                  <p className="card-text">Some quick example textfdslfbusbfusbd;kfh;df di fs iofhsd o;fhsd ifhds fuh ihfp oihiofh[ofiheqfo[qoih[ ih[hto build on the card title and make up the bulk of the card's content.</p>
-                  <a className='btn-primary btn' href='http://www.seriouseats.com/recipes/2017/09/chicken-pot-pie-biscuit-topping-recipe.html'>View Recipe</a>
-                </div>
-            </div>
-          </div>
-          <div className='col-sm-4'>
-            <div className="card">
-              <img className="card-img-top" src="http://www.seriouseats.com/recipes/assets_c/2017/09/20170902-chicken-pot-pie-vicky-wasik40-thumb-1500xauto-439266.jpg" alt="Card image cap"></img>
-                <div className="card-body">
-                  <p className="card-text">Some quick example textfdslfbusbfusbd;kfh;df di fs iofhsd o;fhsd ifhds fuh ihfp oihiofh[ofiheqfo[qoih[ ih[hto build on the card title and make up the bulk of the card's content.</p>
-                  <a className='btn-primary btn' href='http://www.seriouseats.com/recipes/2017/09/chicken-pot-pie-biscuit-topping-recipe.html'>View Recipe</a>
-                </div>
-            </div>
-          </div>
+              <tr>
+                <td ><h5 className='sectionTitle'>More Techniques</h5></td>
+              </tr>
+
+          </table>
         </div>
+        </div>
+
+
 
         <a name='beef'></a>
         <div className='appMain row'>
@@ -107,14 +180,6 @@ class App extends Component {
       </div>
 
 
-
-      <footer>
-        <ul>
-          <li>nfdiub</li>
-          <li>nfdiub</li>
-        </ul>
-      </footer>
-    </div>
 
 
 
--- food/src/food_card/getTitle.js
+++ food/src/food_card/getTitle.js
@@ -1,11 +1,25 @@
-import react, {Component} from 'react';
+var Hello = React.createClass({
+    render: function() {
+      var names = ['Jake', 'Jon', 'Thruster'];
+        var namesList = names.map(function(name, index){
+                  return <li key={ index }>{name}</li>;
+                })
 
-let title = ['food', 'I want', 'to eat', 'now'];
+        return  <ul>{ namesList }</ul>
+    }
+});
 
-var cardTitle = () =>{
-  for (let i = 0; i < 3; i++){
-    title[i];
-  }
-}
+ReactDOM.render(<Hello />, document.getElementById('container'));
 
+<div className='row'>
+  <div className='col-lg-2'>Humorous eats</div>
+  <div className='col-lg-2'></div>
+  <div className='col-lg-1'>Recipes</div>
+  <div className='col-lg-1'>Techniques</div>
+  <div className='col-lg-1'>Features</div>
+  <div className='col-lg-1'>Editors</div>
+  <div className='col-lg-2'>Kitchen Essentials</div>
+  <div className='col-lg-1'>Podcasts</div>
+  <div className='col-lg-1'>search</div>
+</div>
 
