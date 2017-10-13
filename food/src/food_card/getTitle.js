627c8e690196b145fb9445de720489e857281753
Troy Williams <troywilliams@Troys-MBP.hsd1.mn.comcast.net>
2017-10-13 12:38:29 -0500

    idk

--- food/src/food_card/getTitle.js
+++ food/src/food_card/getTitle.js
@@ -1,25 +1,30 @@
-var Hello = React.createClass({
-    render: function() {
-      var names = ['Jake', 'Jon', 'Thruster'];
-        var namesList = names.map(function(name, index){
-                  return <li key={ index }>{name}</li>;
-                })
+function modulo(num1, num2) {
+  var dividend = num1;
+  var divisor = num2;
 
-        return  <ul>{ namesList }</ul>
-    }
-});
 
-ReactDOM.render(<Hello />, document.getElementById('container'));
+  if ( divisor === 0 ) {
+    return 0;
+  }
 
-<div className='row'>
-  <div className='col-lg-2'>Humorous eats</div>
-  <div className='col-lg-2'></div>
-  <div className='col-lg-1'>Recipes</div>
-  <div className='col-lg-1'>Techniques</div>
-  <div className='col-lg-1'>Features</div>
-  <div className='col-lg-1'>Editors</div>
-  <div className='col-lg-2'>Kitchen Essentials</div>
-  <div className='col-lg-1'>Podcasts</div>
-  <div className='col-lg-1'>search</div>
-</div>
+  if ( divisor < 0 ) {
+    divisor = divisor * -1;
+  }
 
+  if ( dividend < 0 ) {
+    dividend = dividend * -1;
+  }
+
+  var i = 1;
+  var product = 0;
+
+  while ( product <= dividend ) {
+    product = divisor * i;
+    i++;
+  }
+
+  //remainder should be 1 because it is one whole number away from the answer
+  return dividend - (product - divisor);
+}
+
+console.log(modulo(25,4));
